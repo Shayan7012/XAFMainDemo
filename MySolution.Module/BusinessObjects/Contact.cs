48e07563561681dd615517cd73754c345fbd9e40
S.Seifi <s.seifi@git.com>
2025-05-05 13:40:22 +0330

    First Commit at the end of Business Model Design

---
+++ MySolution.Module/BusinessObjects/Contact.cs
@@ -0,0 +1,114 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+
+namespace MySolution.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Contact : Person
+    {
+        public Contact(Session session) : base(session) { }
+        private string webPageAddress;
+        public string WebPageAddress
+        {
+            get { return webPageAddress; }
+            set { SetPropertyValue("WebPageAddress", ref webPageAddress, value); }
+        }
+        private string nickName;
+        public string NickName
+        {
+            get { return nickName; }
+            set { SetPropertyValue("NickName", ref nickName, value); }
+        }
+        private string spouseName;
+        public string SpouseName
+        {
+            get { return spouseName; }
+            set { SetPropertyValue("SpouseName", ref spouseName, value); }
+        }
+        private TitleOfCourtesy titleOfCourtesy;
+        public TitleOfCourtesy TitleOfCourtesy
+        {
+            get { return titleOfCourtesy; }
+            set { SetPropertyValue("TitleOfCourtesy", ref titleOfCourtesy, value); }
+        }
+        private DateTime anniversary;
+        public DateTime Anniversary
+        {
+            get { return anniversary; }
+            set { SetPropertyValue("Anniversary", ref anniversary, value); }
+        }
+        private string notes;
+        [Size(4096)]
+        public string Notes
+        {
+            get { return notes; }
+            set { SetPropertyValue("Notes", ref notes, value); }
+        }
+        private Department department;
+        [Association("Department-Contacts")]
+        public Department Department
+        {
+            get { return department; }
+            set { SetPropertyValue("Department", ref department, value); }
+        }
+        private Position position;
+        public Position Position
+        {
+            get { return position; }
+            set { SetPropertyValue("Position", ref position, value); }
+        }
+        [Association("Contact-DemoTask")]
+        public XPCollection<DemoTask> Tasks
+        {
+            get { return GetCollection<DemoTask>("Tasks"); }
+        }
+        private Contact manager;
+        [DataSourceProperty("Department.Contacts")]
+        [DataSourceCriteria("Position.Title = 'Manager' AND Oid != '@This.Oid'")]
+        public Contact Manager
+        {
+            get { return manager; }
+            set { SetPropertyValue("Manager", ref manager, value); }
+        }
+    }
+    [DefaultClassOptions]
+    [System.ComponentModel.DefaultProperty("Title")]
+    public class Department : BaseObject
+    {
+        public Department(Session session) : base(session) { }
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { SetPropertyValue("Title", ref title, value); }
+        }
+        private string office;
+        public string Office
+        {
+            get { return office; }
+            set { SetPropertyValue("Office", ref office, value); }
+        }
+        [Association("Department-Contacts")]
+        public XPCollection<Contact> Contacts
+        {
+            get { return GetCollection<Contact>("Contacts"); }
+        }
+    }
+    [DefaultClassOptions]
+    [System.ComponentModel.DefaultProperty("Title")]
+    public class Position : BaseObject
+    {
+        public Position(Session session) : base(session) { }
+        private string title;
+        [RuleRequiredField(DefaultContexts.Save)]
+        public string Title
+        {
+            get { return title; }
+            set { SetPropertyValue("Title", ref title, value); }
+        }
+    }
+    public enum TitleOfCourtesy { Dr, Miss, Mr, Mrs, Ms };
+}
---
+++ MySolution.Module/BusinessObjects/Task.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace MySolution.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [ModelDefault("Caption", "Task")]
+    public class DemoTask : Task
+    {
+        public DemoTask(Session session) : base(session) { }
+        [Association("Contact-DemoTask")]
+        public XPCollection<Contact> Contacts
+        {
+            get { return GetCollection<Contact>("Contacts"); }
+        }
+        private Priority priority;
+        public Priority Priority
+        {
+            get { return priority; }
+            set { SetPropertyValue("Priority", ref priority, value); }
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            Priority = Priority.Normal;
+        }
+    }
+    public enum Priority
+    {
+        Low = 0,
+        Normal = 1,
+        High = 2
+    }
+}
