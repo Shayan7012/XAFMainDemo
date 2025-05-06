82936fd8daf1652234625648823afd5662f9dc6d
S.Seifi <s.seifi@git.com>
2025-05-06 16:22:55 +0330

    Access Grid Control Properties

---
+++ MySolution.Module.Web/Controllers/WebAlternatingRowsController.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.ExpressApp;
+using System;
+using System.Drawing;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+
+namespace MySolution.Module.Web.Controllers
+{
+    public partial class WebAlternatingRowsController : ViewController
+    {
+        public WebAlternatingRowsController()
+        {
+            InitializeComponent();
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+        }
+        protected override void OnDeactivated()
+        {
+            base.OnDeactivated();
+        }
+        private void WebAlternatingRowsController_ViewControlsCreated(object sender, EventArgs e)
+        {
+            ASPxGridListEditor listEditor = ((ListView)View).Editor as ASPxGridListEditor;
+            if (listEditor != null) listEditor.Grid.Styles.AlternatingRow.BackColor = Color.FromArgb(244, 244, 244);
+        }
+    }
+}
